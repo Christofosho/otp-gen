7a8a18ea8b538b98e51dce170998ddbe2943dbf4
Christopher Snow <csnow@live.ca>
2026-02-23 14:24:07 -0500

    Apply suggestion from @Christofosho

--- tests/test_util.py
+++ tests/test_util.py
@@ -24,8 +24,6 @@ class TestUtil(unittest.TestCase):
                 "--language",
                 "typescript",
                 "--dc-files",
-                "test.dc",
-                "test2.dc",
                 "sample.dc",
                 "sample2.dc",
                 "build",
